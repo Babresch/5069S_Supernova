bbd037919489f381ad9bb7e3e8e7dff3d546d79d
Babresch <ben.d.abresch@gmail.com>
2026-01-15 00:02:35 -0600

    Change comment from 'yo yo yo' to 'Howdy'

--- OneDrive/Documents/vexcode-projects/SlopeBot-Code-5069S/src/main.cpp
+++ OneDrive/Documents/vexcode-projects/SlopeBot-Code-5069S/src/main.cpp
@@ -45,7 +45,7 @@ motor_group L= motor_group(FL, ML, BL);
 ╚═╝░░╚═╝░╚════╝░╚═════╝░░╚════╝░░░░╚═╝░░░  ░╚════╝░░╚════╝░╚═╝░░╚══╝░░░╚═╝░░░╚═╝░░╚═╝░╚════╝░╚══════╝╚═════╝░
 */
 
-  //yo yo yo
+  //Howdy
   int R_percentage = 0;
   int L_percentage = 0;
   int axis_R = 0;
