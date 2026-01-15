f40dbf830f661a4b52a847e6951024d77e4aa11c
Babresch <ben.d.abresch@gmail.com>
2026-01-14 23:46:23 -0600

    Update comment for clarity in main.cpp

--- OneDrive/Documents/vexcode-projects/SlopeBot-Code-5069S/src/main.cpp
+++ OneDrive/Documents/vexcode-projects/SlopeBot-Code-5069S/src/main.cpp
@@ -45,7 +45,7 @@ motor_group L= motor_group(FL, ML, BL);
 ╚═╝░░╚═╝░╚════╝░╚═════╝░░╚════╝░░░░╚═╝░░░  ░╚════╝░░╚════╝░╚═╝░░╚══╝░░░╚═╝░░░╚═╝░░╚═╝░╚════╝░╚══════╝╚═════╝░
 */
 
-  //test test
+  //test test test
   int R_percentage = 0;
   int L_percentage = 0;
   int axis_R = 0;
